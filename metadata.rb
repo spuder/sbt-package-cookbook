981ea25a59d819e16bff285c585edbbf7b1db6de
Spencer Owen <owenspencer@gmail.com>
2015-11-24 09:35:24 -0700

    Updates metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'sbt-package'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Spencer Owen'
+maintainer_email 'owenspencer@gmail.com'
 license 'all_rights'
 description 'Installs/Configures sbt-package'
 long_description 'Installs/Configures sbt-package'
